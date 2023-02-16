bc129683fc6aaf60ccc44a79031aa3e7e45f3e42
Cristian Reynés <cristianreynes@gmail.com>
2023-02-16 12:35:20 -0300

    Add mailspring install to fedora-postinstall.sh

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -16,6 +16,10 @@ sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 # Install packages
 sudo dnf -y install bat cmatrix cmus codium exa feh gcc-c++ git kitty mpv libreoffice neofetch neovim obs-studio python3-pip qbittorrent redshift rofi steam-devices stow syncthing tldr unrar vifm vlc VirtualBox WoeUSB xkill zathura zathura-pdf-poppler
+# Download mailspring rpm
+wget https://updates.getmailspring.com/download?platform=linuxRpm -O ~/Downloads/mailspring.rpm
+# Install mailspring rpm
+sudo dnf -y install ~/Downloads/mailspring.rpm
 
 # ~/.local/bin/
 pip install --user bumblebee-status
