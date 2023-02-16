9a602c109393955139dc12e4fafcca76cccc45ca
Cristian Reynés <cristianreynes@gmail.com>
2023-02-16 12:09:59 -0300

    Add VSCodium repo & package fedora-i3-postinstall.sh

--- fedora-i3-postinstall.sh
+++ fedora-i3-postinstall.sh
@@ -10,8 +10,12 @@ sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # Update system
 sudo dnf -y update 
+# Add VSCodium repo GPG key
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+# Add VSCodium repo
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 # Install packages
-sudo dnf -y install bat cmatrix cmus exa feh gcc-c++ git kitty mpv libreoffice neofetch neovim obs-studio python3-pip qbittorrent redshift rofi steam-devices stow syncthing tldr unrar vifm vlc VirtualBox WoeUSB xkill zathura zathura-pdf-poppler
+sudo dnf -y install bat cmatrix cmus codium exa feh gcc-c++ git kitty mpv libreoffice neofetch neovim obs-studio python3-pip qbittorrent redshift rofi steam-devices stow syncthing tldr unrar vifm vlc VirtualBox WoeUSB xkill zathura zathura-pdf-poppler
 
 # ~/.local/bin/
 pip install --user bumblebee-status
