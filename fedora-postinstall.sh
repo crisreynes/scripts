c6fbd65b356a865270f3f50f17a4346cf4c274c1
crisreynes <cristianreynes@gmail.com>
2023-09-01 19:00:14 -0300

    Update packages list

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -26,7 +26,7 @@ rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/mast
 sudo -u $SUDO_USER printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 
 # Install packages
-dnf -y install bat cmatrix codium exa flameshot flatpak git kitty mpv libreoffice neofetch neovim obs-studio picom qbittorrent redshift rofi steam-devices stow syncthing tldr unrar vifm vlc VirtualBox WoeUSB xkill zathura zathura-pdf-poppler
+dnf -y install bat cmatrix codium exa flameshot flatpak git kitty mpv libreoffice neofetch neovim newsboat obs-studio picom qbittorrent redshift rofi steam-devices stow syncthing tldr unrar vifm vlc VirtualBox WoeUSB xkill zathura zathura-pdf-poppler
 dnf -y install cargo dbus-devel gcc-c++ libxcb-devel ncurses-devel openssl-devel pulseaudio-libs-devel python3-pip rust
 # Download mailspring rpm
 sudo -u $SUDO_USER wget https://updates.getmailspring.com/download?platform=linuxRpm -O /home/"$SUDO_USER"/Downloads/mailspring.rpm
