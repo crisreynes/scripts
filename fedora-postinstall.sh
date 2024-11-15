5fa62dc9a213e6fec00ea3eb187660873c253466
Cristian Reynés <cristianreynes@gmail.com>
2024-11-15 17:39:05 -0300

    Update RPM Fusion commands

--- fedora-postinstall.sh
+++ fedora-postinstall.sh
@@ -23,12 +23,7 @@ bash -c "echo 'keepcache=True' >> /etc/dnf/dnf.conf"
 dnf -y update
 # Enable RPM Fusion
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y config-manager --enable fedora-cisco-openh264
-# Switch to full ffmpeg
-dnf -y swap ffmpeg-free ffmpeg --allowerasing
-# Install additional codec
-dnf -y groupupdate multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-dnf -y groupupdate sound-and-video
+dnf -y config-manager setopt fedora-cisco-openh264.enabled=1
 
 # Install packages
 dnf -y install bat cmatrix emacs eza fastfetch ffmpegthumbnailer flameshot flatpak git go kitty mpv libreoffice neovim newsboat picom qbittorrent qtile redshift rofi sddm snapd stow syncthing tldr unrar vlc WoeUSB xkill zathura zathura-pdf-poppler
